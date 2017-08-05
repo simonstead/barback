2e8c37c1abd2ccbce963be725d07227430f8c0e7
Simon Stead <simonstead@me.com>
2017-08-05 08:42:21 +0100

    add test files

---
+++ test/tests.js
@@ -0,0 +1,9 @@
+const expect = require('expect')
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      expect([1,2,3].indexOf(4)).toBe(-1);
+    });
+  });
+});
